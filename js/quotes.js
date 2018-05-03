8eee9673fb1910ae7db7ee6357c7526010dbf572
decp88 <declan_parnell@hotmail.com>
2018-05-03 19:42:58 +0800

    Update 2

--- js/quotes.js
+++ js/quotes.js
@@ -1,11 +1,11 @@
 var quotes = [
     {quote: 'Luck is what happens when preparation meets opportunity.',
     author: 'Seneca',
-    year: '100 AD' 
+    year: '100 AD', 
     },
     {quote:'Everything we hear is an opinion, not a fact. Everything we see is a perspective, not the truth.',
     author: 'Marcus Aurelius',
-    year:''
+     year: "0",
     },
     {quote:'I am the wisest man alive, for I know one thing, and that is that I know nothing.',
     author: 'Plato',
@@ -13,11 +13,11 @@ var quotes = [
     },
     {quote:'Not what we have But what we enjoy, constitutes our abundance',
     author: 'Epicurus',
-    year: ' '
+    year: "0",
     },
     {quote:'Do not spoil what you have by desiring what you have not; remember that what you now have was once among the things you only hoped for.',
     author: 'Seneca',
-    year: '5 BC'
+    year:"0",
     },
     {quote:'Peace out',
     author: 'Declan',
--- js/script.js
+++ js/script.js
@@ -1,28 +1,41 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
+
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 
 var quote;
 var author;
-var randomNum = Math.round(Math.random() * 4 +1);
+var yearWrote;
 
-function getRandomQuote(randomNumber) {
-    var selectedRandomQuote = quotes[randomNumber];
-    quote = selectedRandomQuote.quote;
-    author = selectedRandomQuote.author;
+
+
+function getRandomQuote() {
+    var randomNum = Math.round(Math.random() * 4 +1);
+    var selectedRandomQuote = quotes[randomNum];
     
     return selectedRandomQuote;
 }
 
 
 function printQuote () {
-    var pickedQuote = getRandomQuote(randomNum);
+    var pickedQuote = getRandomQuote();
     quote = pickedQuote.quote;
     author = pickedQuote.author;
+    yearWrote = pickedQuote.year;
+    
     
     document.getElementById('quote').innerHTML = quote;
     document.getElementById('source').innerHTML = author;
-    
+    document.getElementById('year').innerHTML = yearWrote;
 }
+    
+    /*if(year !== "0") {
+        
+    }
+    else {
+        document.getElementById('year').innerHTML = "year not known";
+    }*/
+    
+
 
