8e7492eb0ddcebedbd54afc0542b50161a5fd86a
decp88 <declan_parnell@hotmail.com>
2018-05-05 07:51:17 +0800

    Update 3
    Added change to background colour with each quote.
    Added automatic transition every 12 seconds between quotes.

--- js/quotes.js
+++ js/quotes.js
@@ -4,34 +4,41 @@ var quotes = [
     author: 'Seneca',
     year: '100 AD', 
     citation: 1,
+    tag: '#Inspiration'
     },
     {quote:'Everything we hear is an opinion, not a fact. Everything we see is a perspective, not the truth.',
     author: 'Marcus Aurelius',
     year: "300 BC",
     citation: 'Marcus Aurelius - Meditations',
+    tag: '#Inspiration',
+    color: "blue",
     },
     {
     quote:'I am the wisest man alive, for I know one thing, and that is that I know nothing.',
     author: 'Plato',
     year: '10AD',
     citation: 1,
+    tag: '#Smart'
     },
     {
-    quote:'Not what we have But what we enjoy, constitutes our abundance',
+    quote:'Not what we have, But what we enjoy, constitutes our abundance',
     author: 'Epicurus',
     year: "100BC",
     citation: 1,
+    tag: '#Life',
     },
     {
     quote:'Do not spoil what you have by desiring what you have not; remember that what you now have was once among the things you only hoped for.',
     author: 'Seneca',
     year: "200BC",
     citation: 'Seneca, the art of living',
+    tag: '#Inspiration',
     },
     {
     quote:'Do not spoil what you have by desiring what you have not; remember that what you now have was once among the things you only hoped for.',
     author: 'Seneca',
     year: 1,
     citation: 1,
+    tag: '#Smart',
     },
 ];
--- js/script.js
+++ js/script.js
@@ -3,12 +3,18 @@
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-
+// Variables
 var quote;
 var author;
 var yearWrote;
 var citation;
+var tag;
 
+// Set interval for quote 12 seconds
+
+setInterval(printQuote, 12000);
+
+//Select random quote
 
 function getRandomQuote() {
     var randomNum = Math.round(Math.random() * 4 +1);
@@ -18,34 +24,61 @@ function getRandomQuote() {
 }
 
 
+// Print quote to screen and change background colour 
+
 function printQuote () {
     var pickedQuote = getRandomQuote();
     quote = pickedQuote.quote;
     author = pickedQuote.author;
     yearWrote = pickedQuote.year;
-    citation = pickedQuote.citation
+    citation = pickedQuote.citation;
+    tag = pickedQuote.tag;
+    
     
     
     var constructCiting = author;
     
-    if(citation != 1) {
+    if(citation !== 1) {
         constructCiting = author;
         constructCiting += "<span class='citation'>" + citation + "</span>";
     }
     
-    if (yearWrote === 1) {
-        constructCiting;
-    }
-    else {
+    if (yearWrote !== 1) {
         constructCiting;
         constructCiting += "<span id='year'>" + yearWrote + "</span>";
     }
     
+    if (tag !== 1) {
+        document.getElementById("tag").innerHTML = tag;
+    }
+ 
     
     document.getElementById('source').innerHTML = constructCiting;
     document.getElementById('quote').innerHTML = quote;
+    document.getElementById('quote-box').style.background = randomRGB();
 
 }
+
+// Generate Random colour for background for each quote
+
+function randomRGB() {
+  var col =
+    "rgb(" +
+    randomColor(255) +
+    "," +
+    randomColor(255) +
+    "," +
+    randomColor(255) +
+    ")";
+  return col;
+}
+
+function randomColor(num) {
+  var randomNumber = Math.round(Math.random() * num);
+  return randomNumber;
+}
+
+
 
 
     
